42da8859c65199146414b7a643132485428ba464
umulsalammohamed <mohamedumusalam@gmail.com>
2026-08-31 08:32:37 -0700

    Add files via upload

---
+++ all_about_me.py
@@ -0,0 +1,10 @@
+# My introduction program
+name = "Umulsalam"
+age = 22
+favourite_language = "Python"
+goal = "to become a skilled software developer"
+
+print(f"Hello, my name is {name}.")
+print(f"I am {age} years old.")
+print(f"My favourite programming language is {favourite_language}.")
+print(f"My goal for this course is {goal}.")
